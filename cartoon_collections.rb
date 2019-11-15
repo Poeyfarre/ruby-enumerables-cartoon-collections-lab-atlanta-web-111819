30c29464e71e4267fd20210433e905f0e0e7991e
Jacy Leech <jacy.atlanta@gmail.com>
2019-11-15 16:02:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,10 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = []
+  array.map do |element|
+    new_array
 end
 
 def long_planeteer_calls# code an argument here
