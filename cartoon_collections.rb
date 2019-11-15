97dbc36d811bd21d3ec7c43a59fdd320cd280800
Jacy Leech <jacy.atlanta@gmail.com>
2019-11-15 16:28:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,9 +13,9 @@ end
 return new_array
 end
 
-# def long_planeteer_calls(array)
-#   array.any? {|call| call.length < 4}
-# end
+def long_planeteer_calls(array)
+  array.any? {|call| call.length < 4}
+end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
